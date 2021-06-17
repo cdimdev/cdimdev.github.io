466ecc6a28ba89279dfc6b5e8acbae9d83c122f7
Cdimdev <cdimdev@gmail.com>
2021-06-17 03:03:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var APP_PREFIX = 'Ergasia3_mai_21010'     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
+  '/main.js',                     // If you have separate JS/CSS files,
+  '/index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
