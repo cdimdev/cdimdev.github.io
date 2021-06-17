08bf1af8a80eac6b5361ab32f0510f4fdacc58a3
Cdimdev <cdimdev@gmail.com>
2021-06-17 05:03:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css"/>
+    <script src="bootstrap-4.3.1-dist/js/jquery.min.js"> </script>
+    <script src="bootstrap-4.3.1-dist/js/bootstrap.bundle.js"> </script>
 self.addEventListener('install', function (event) {
     console.log('The service worker is being installed.');
     event.waitUntil(
@@ -10,10 +13,9 @@ self.addEventListener('install', function (event) {
                 '/style.css',
                 '/icon192.png',
                 '/mail.png',
-                'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+                '/bootstrap-4.3.1-dist/css/bootstrap.min.css',
+                '/bootstrap-4.3.1-dist/js/jquery.min.js',
+                '/bootstrap-4.3.1-dist/js/bootstrap.bundle.js'
             ]);
         })
     );
