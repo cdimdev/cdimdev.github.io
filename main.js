034e95214ceeabeb17a7572da1a7d9da6843b760
Cdimdev <cdimdev@gmail.com>
2021-06-17 03:15:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 'use strict';
-self.addEventListener('install', function (event) {
-  console.log('The service worker is being installed.');
-  event.waitUntil(
-    caches.open('to-do').then(function (cache) {
-      return cache.addAll([
-        '/index.html',
-        '/rs.png',
-        '/mail.png'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', function (event) {
-  console.log('The service worker is serving the asset.');
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || caches.match('/index.html');
-    })
-  );
-});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(function (error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
 
 //Initialize application data
 var dummyParagraph =
