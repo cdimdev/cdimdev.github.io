290fb73cc9af429ebde73cd1b6cba69c2b974239
Cdimdev <cdimdev@gmail.com>
2021-06-17 17:09:08 +0300

    Delete js directory

--- js/startServiceWorker.js
+++
@@ -1,7 +0,0 @@
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').catch(function (error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
-    });
-}
