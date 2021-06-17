bbf46379e520c7d26c671d2434502158d6c38469
Cdimdev <cdimdev@gmail.com>
2021-06-17 04:43:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,11 @@ self.addEventListener('install', function (event) {
                 '/manifest.json',
                 '/style.css',
                 '/icon192.png',
-                '/mail.png'
+                '/mail.png',
+                'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
             ]);
         })
     );
